e293d5d4884eb4a3f363f5336dd920b468002b43
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-12 18:42:00 +0000

    used setTimeout to delay each char and added newline print @ end

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,7 @@
 const sentence = 'hello there from lighthouse labs';
-let t = 
+let t = 0;
 for(const char of sentence){
-  setTimeout(()=>process.stdout.write(char), 1000);
+  setTimeout(()=>process.stdout.write(char), t);
+  t += 50;
 }
+setTimeout(()=>process.stdout.write('\n'), 50*sentence.length);
